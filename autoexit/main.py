09b3f9eea7b60338faaf8de7f7bcfd42731447a9
Joseph Thurlow <joseph.thurlow7d0@gmail.com>
2024-09-07 00:09:15 +1000

    renamed src folder

--- autoexit/main.py
+++ autoexit/main.py
@@ -8,9 +8,7 @@ class Stocker(App):
     @work
     async def on_mount(self) -> None:
         self.screen.styles.background = "#23232b"
-
         api_service = await self.push_screen_wait(WelcomeView())
-
         self.push_screen(DashboardView(api_service=api_service))
         
 
